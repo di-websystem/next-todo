7b264d292f4cd462de1f458c77d1f2bb7721fe7c
tatsuo <you@test.com>
2021-01-20 15:37:54 +0900

    fix: config-pwa

--- next.config.js
+++ next.config.js
@@ -7,6 +7,9 @@ const runtimeCaching = require('next-pwa/cache');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    sw: 'service-worker.js',
     runtimeCaching,
   },
 });
