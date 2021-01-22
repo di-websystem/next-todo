89290f1cf21ed186f25c14f07aa276358999bc91
tatsuo <you@test.com>
2021-01-22 15:24:10 +0900

    fix: import-error example-storybook

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,6 +1,5 @@
+import type { Story, Meta } from '@storybook/react';
 import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Button, ButtonProps } from './Button';
 
